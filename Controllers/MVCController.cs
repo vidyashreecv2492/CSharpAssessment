208b1ddfacccbe70c1ffa253270119cf12616e51
vidyashree c v <69243283+vidyashreecv2492@users.noreply.github.com>
2020-10-03 03:32:29 -0700

    Add files via upload

---
+++ Controllers/MVCController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ExamWebApp.Models;
+
+namespace ExamWebApp.Controllers
+{
+    public class MVCController : Controller
+    {
+        public ViewResult Display()
+        {
+            var context = new MyDatabase();
+            var model = context.CustTables.ToArray();
+            return View(model);
+        }
+        public ActionResult Find(string id)
+        {
+            int CustId = int.Parse(id);
+            var context = new MyDatabase();
+            var model = context.custTables.FirstOrDefault((e) => e.CustId == CustId);
+            return View(model);
+        }
+      
+        public ActionResult Find(CustTable customer)
+        {
+            var context = new MyDatabase();
+            var model = context.custTables.FirstOrDefault((e) => e.CustId == Customer.CustId);
+            model.CustName = customer.EmpName;
+            model.CustAddress = customer.EmpAddress;
+            model.CustSalary = customer.EmpSalary;
+            context.SaveChanges();
+            return RedirectToAction("Display");
+        }
+        public ViewResult AddCustomer()
+        {
+            var model = new CustTable();
+            return View(model);
+        }
+        public ActionResult AddCustomer(CustTable emp)
+        {
+            var context = new MyDatabase();
+            context.CustTables.Add(Customer);
+            context.SaveChanges();
+            return RedirectToAction("Display");
+        }
+        public ActionResult DeleteCustomer(string id)
+        {
+            int EmpId = int.Parse(id);
+            var context = new MyDatabase();
+            var model = context.EmpTables.FirstOrDefault((e) => e.CustID == CustId);
+            context.CustTables.Remove(model);
+            context.SaveChanges();
+            return RedirectToAction("Display");
+        }
+    }
+}
---
+++ Models/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExamWebApp.Models
+{
+    public class Customer
+    {
+        public int CustId { get; set; }
+        public string CustName { get; set; }
+        public string CustAddress { get; set; }
+        public string CustManager { get; set; }
+
+    }
+    public override string ToString()
+        {
+            return $"<p>Customer Id: {CustId}</P><p>Customer Name: {CustName}</p><p>Customer Address: {CustAddress}</p><p>Customer Manager: {CustManager}</p>";
+        }
+}
